58ea2714da9a93aa5c7dce768eefbca8190a9446
Saute0212 <unknown@a.com>
2024-02-25 00:13:48 +0900

    Edit "main.py"

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ DataBits = [0, 0, 0, 0, 0, 0, 0, 0,
             0, 0, 0, 0, 0, 0, 0, 0,
             0, 0, 0, 0, 0, 0, 0, 0,
             0, 0, 0, 0, 0, 0, 0, 0]
-HIGH_Humidity = [0, 0, 0, 0, 0, 0, 0, 0]
-LOW_Humidity = [0, 0, 0, 0, 0, 0, 0, 0]
-HIGH_Temperature = [0, 0, 0, 0, 0, 0, 0, 0]
-LOW_Temperature = [0, 0, 0, 0, 0, 0, 0, 0]
-Parity_bit = [0, 0, 0, 0, 0, 0, 0, 0]
+# HIGH_Humidity = [0, 0, 0, 0, 0, 0, 0, 0]
+# LOW_Humidity = [0, 0, 0, 0, 0, 0, 0, 0]
+# HIGH_Temperature = [0, 0, 0, 0, 0, 0, 0, 0]
+# LOW_Temperature = [0, 0, 0, 0, 0, 0, 0, 0]
+# Parity_bit = [0, 0, 0, 0, 0, 0, 0, 0]
 Humidity = 0
 Temperature = 0
 Parity = 0
@@ -21,3 +21,8 @@ Parity = 0
 # main
 while True:
     dht11_orglib.init_dht11(DATA_PIN)
+    for i in 40:
+        DataBits[i] = dht11_orglib.data_get(DATA_PIN)
+    for j in 40:
+        print("%d : Data: %d", j, DataBits[j])
+    
