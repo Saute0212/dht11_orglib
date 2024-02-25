9fbc7d421981bb30b9f3433c61923cabc7143e33
Saute0212 <unknown@a.com>
2024-02-25 17:22:27 +0900

    Edit "main.py"

--- main.py
+++ main.py
@@ -17,27 +17,28 @@ Temperature = 0
 # main
 while True:
     if Flag == 0:
-        # Initialization
         Flag = 1
+        
+        # Initialization
         dht11_orglib.init_dht11(DATA_PIN)
-    else:
-        # Get data
-        if dht11_orglib.data_get(DATA_PIN) == 2:
-            for i in range(40):
-                BitsData[i] = dht11_orglib.data_get(DATA_PIN)
+
+    # Get data
+    if dht11_orglib.data_get(DATA_PIN) == 2:
+        for i in range(40):
+            BitsData[i] = dht11_orglib.data_get(DATA_PIN)
     
-            # Convert data(bit to decimal)
-            dht11_orglib.convert(BitsData, DecimalData)
+        # Convert data(bit to decimal)
+        dht11_orglib.convert(BitsData, DecimalData)
     
-            # Check data
-            if dht11_orglib.checksum(BitsData) == 0:
-                # Correct data
-                Humidity = 0
-                Temperature = 0
-                print("Humidity:", Humidity, "Temperature:", Temperature)
-            else:
-                # Not correct data
-                print("Error")
+        # Check data
+        if dht11_orglib.checksum(BitsData) == 0:
+            # Correct data
+            Humidity = 0
+            Temperature = 0
+            print("Humidity[%]:", Humidity, "Temperature[℃]:", Temperature)
+        else:
+            # Not correct data
+            print("Error")
             
-            # Reset Falg
-            Flag = 0
+        # Reset Falg
+        Flag = 0
