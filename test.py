7f9badcdbaf4c5d18126560605855b4395dfd714
Saute0212 <unknown@a.com>
2024-03-01 01:28:11 +0900

    20240301_0

--- test.py
+++ test.py
@@ -1,41 +1,36 @@
-import RPi.GPIO as GPIO
 import time
+import pigpio
 
-# GPIOピンの設定
-GPIO_PIN = 17
+PIN = 17
 
-# GPIOの初期化
-GPIO.setmode(GPIO.BCM)
-# GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+pi = pigpio.pi()
 
-# 割り込み発生時のコールバック関数
-def interrupt_callback(channel):
-    if GPIO.input(channel):
-        print("LOW->HIGH")
-    else:
-        print("HIGH->LOW")
-
-def init_dht11(SelectPin):
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(SelectPin, GPIO.OUT)
-    GPIO.output(SelectPin, GPIO.HIGH)
-    delay_time(0.5)
-    GPIO.output(SelectPin, GPIO.LOW)
-    delay_time(0.02)
-    GPIO.output(SelectPin, GPIO.HIGH)
-    GPIO.setup(SelectPin, GPIO.IN)
-
-# Stops program for requested time (depends on system time)
 def delay_time(DelayTime):
     StartTime = time.monotonic()
     ElapsedTime = 0
     while ElapsedTime < DelayTime:
         ElapsedTime = time.monotonic() - StartTime
 
-# 割り込みイベントの設定
-GPIO.add_event_detect(GPIO_PIN, GPIO.BOTH, callback=interrupt_callback)
+def init(pin):
+    pi.set_mode(pin, pigpio.OUTPUT)
+    pi.write(pin, 1)
+    delay_time(0.5)
+    pi.write(pin, 0)
+    delay_time(0.02)
+    pi.write(pin, 1)
+    delay_time(0.01)
+    pi.set_mode(pin, pigpio.INPUT)
 
-print("waiting...")
+    while True:
+        if pi.read(pin) == 0:
+            break
+    while True:
+        if pi.read(pin) == 1:
+            break
+    while True:
+        if pi.read(pin) == 0:
+            break
+    return 1
+
 while True:
-    init_dht11(GPIO_PIN)
-    delay_time(10)
+    print(init(PIN))
